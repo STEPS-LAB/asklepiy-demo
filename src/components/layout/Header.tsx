9d91f58b2eb57609adf18e80f70dd62b4632d21f
alexsomeone1 <93034733+alexsomeone1@users.noreply.github.com>
2026-03-09 14:23:20 +0200

    збільшив лого

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -59,7 +59,7 @@ export function Header() {
             {/* Logo */}
             <Link href="/" className="group">
               <motion.div
-                className="relative w-[150px] h-[80px]"
+                className="relative w-[195px] h-[104px]"
                 whileHover={{ scale: 1.05 }}
                 transition={{ type: 'spring', stiffness: 400, damping: 17 }}
               >
